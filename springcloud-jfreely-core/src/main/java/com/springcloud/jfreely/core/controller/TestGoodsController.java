9fc756aa1de7ab4199acded0644a0b54ac1d89c6
qjx <393657562@qq.com>
2018-12-12 09:45:42 +0800

    添加goodsController

--- springcloud-jfreely-core/src/main/java/com/springcloud/jfreely/core/controller/TestGoodsController.java
+++ springcloud-jfreely-core/src/main/java/com/springcloud/jfreely/core/controller/TestGoodsController.java
@@ -1,23 +1,21 @@
 package com.springcloud.jfreely.core.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alibaba.fastjson.JSONObject;
+import com.springcloud.jfreely.core.client.IGoodsClient;
 
 @RestController
 public class TestGoodsController {
+	@Autowired
+	private IGoodsClient iGoodsClient;
 	
 	@RequestMapping(value = "/getGoodsInfo")
 	@ResponseBody
-	public JSONObject getGoodsInfo () {
-		return new JSONObject() {
-			{
-				put("id" , 1);
-				put("goodsName" , "Air 1");
-				put("goodsType"  , "鞋");
-			}
-		};
+	public JSONObject getGoodsInfo() {
+		return iGoodsClient.getGoodsInfo();
 	}
 }
